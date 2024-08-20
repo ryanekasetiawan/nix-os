0b00b36a41abf4fa5e537ef54a2d08f1899b62c2
Ryan Eka Setiawan <setiawanryaneka@gmail.com>
2024-08-21 06:47:14 +0700

    Create dev.nix

---
+++ dev.nix
@@ -0,0 +1,70 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    # pkgs.go
+    # pkgs.python311
+    # pkgs.python311Packages.pip
+    pkgs.nodejs_20
+    # pkgs.nodePackages.nodemon
+    pkgs.openssh
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+      "anseki.vscode-color"
+      "christian-kohler.path-intellisense"
+      "Codeium.codeium"
+      "dbaeumer.vscode-eslint"
+      "dsznajder.es7-react-js-snippets"
+      "esbenp.prettier-vscode"
+      "formulahendry.auto-rename-tag"
+      "mhutchie.git-graph"
+      "mrmlnc.vscode-scss"
+      "oderwat.indent-rainbow"
+      "steoates.autoimport"
+      "bradlc.vscode-tailwindcss"
+      "ms-vscode.js-debug"
+      "ms-vscode.vscode-typescript-next"
+      "rangav.vscode-thunder-client"
+      "Zignd.html-css-class-completion"
+    ];
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        # web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "dev"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+    };
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+        # Open editors for the following files by default, if they exist:
+        # default.openFiles = [ ".idx/dev.nix" "README.md" ];
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
